74ee79aef5bed4c01b935229453fd0450f88b285
Igor <igorer27@yandex.ru>
2017-10-09 02:24:00 +0300

    only 14 tests done

--- src/main/java/ru/mail/polis/KVServiceFactory.java
+++ src/main/java/ru/mail/polis/KVServiceFactory.java
@@ -1,6 +1,7 @@
 package ru.mail.polis;
 
 import org.jetbrains.annotations.NotNull;
+import ru.mail.polis.erofeevskiy_ia.MyFileDAO;
 import ru.mail.polis.erofeevskiy_ia.MyService;
 
 import java.io.File;
@@ -45,6 +46,6 @@ final class KVServiceFactory {
             throw new IllegalArgumentException("Path is not a directory: " + data);
         }
 
-        return new MyService(port);
+        return new MyService(port, new MyFileDAO(data));
     }
 }
